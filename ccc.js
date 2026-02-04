43b6f17de6a7c68c3b16bd00b8ccaf06e5d47bc1
Keith Creelman <keithcreelman@Keiths-MacBook-Air.local>
2026-02-03 23:38:57 -0500

    Send contract_status with MYM submit payload

--- ccc.js
+++ ccc.js
@@ -661,6 +661,7 @@
     YEAR: String(YEAR),
     aav: safeInt(calc.aav),
     contract_info: String(calc.contractInfo),
+    contract_status: "A",
     contract_year: safeInt(years),
     guaranteed: safeInt(calc.gtd),
     leagueId: String(L),

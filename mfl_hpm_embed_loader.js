650312fed51a88fa033d16eaa0ebd588766ffd8a
Keith Creelman <keithcreelman@Keiths-MacBook-Air.local>
2026-02-13 15:56:46 -0500

    Bump CCC cache version for MFL embed

--- mfl_hpm_embed_loader.js
+++ mfl_hpm_embed_loader.js
@@ -65,7 +65,7 @@
 
   const LATEST_JSON_URL = "https://keithcreelman.github.io/ups-league-data/ccc_latest.json";
   const LATEST_JS_URL = "https://keithcreelman.github.io/ups-league-data/ccc_latest.js";
-  const DEFAULT_CACHE = "20260209ac";
+  const DEFAULT_CACHE = "20260213c";
 
   let mount = document.getElementById("cccMount");
   if (!mount) {
